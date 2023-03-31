a70f4fc70e48ffea5f3e8b444f8c909e366397be
danielmedtar <dmedinat.ar@gmail.com>
2023-03-31 16:16:31 -0300

    adding functionalities

--- js/app.js
+++ js/app.js
@@ -1,28 +1,46 @@
-const sueldoBruto = document.getElementById('sueldo-bruto');
-const bonos = document.getElementById('bonos');
+const sueldoBruto = document.querySelector('.sueldo-bruto');
+const bonos = document.querySelector('.bonos');
 const sueldoNeto = document.querySelector('.resultado');
 const calcular = document.getElementById('calcular');
 const limpiar = document.getElementById('limpiar')
 
+const formateador = new Intl.NumberFormat(undefined, {
+    style: 'currency',
+    currency: 'ARS',
 
-function puntoNumeros(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-}
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+})
 
 calcular.addEventListener('click', (e) => {
     e.preventDefault();
-    function hacerCalculo() {
-        if(bonos.value !== '') {
-            let resultado = (parseFloat(sueldoBruto.value) + parseFloat( bonos.value)) * 0.83;
-            sueldoNeto.innerHTML = `Tu sueldo neto es de ${puntoNumeros(resultado)}`
-        } else {
-            let resultado = (parseFloat(sueldoBruto.value)) * 0.83;
-            sueldoNeto.innerHTML = `Tu sueldo neto es de ${puntoNumeros(resultado)}`
-        }
-    }
 
-    hacerCalculo();
-} )
+    function validar() {
+        if(sueldoBruto.value == '' || sueldoBruto.value <= 0 || isNaN(sueldoBruto.value)) {
+            sueldoBruto.classList.add ('error')
+            sueldoNeto.innerHTML = `Ingresa un sueldo válido`
+            sueldoNeto.style.color = 'red'
+        } else {
+                function hacerCalculo() {
+                    if(bonos.value !== '') {
+                        let resultado = ((parseFloat(sueldoBruto.value) + parseFloat( bonos.value)) * 0.83).toFixed(2);
+                        sueldoNeto.innerHTML = `Tu sueldo neto es <span class="bold">${formateador.format(resultado)}</span>`
+    
+                        sueldoNeto.style.color = 'white'
+                    } else {
+                        let resultado = ((parseFloat(sueldoBruto.value)) * 0.83).toFixed(2);
+                        sueldoNeto.innerHTML = `Tu sueldo neto es <span class="bold">${formateador.format(resultado)}</span>`
+    
+                        sueldoNeto.style.color = 'white'
+                    }
+                }
+
+            }
+            hacerCalculo();
+        }
+        validar()
+    }
+)
 
 limpiar.addEventListener('click', () => {
     location.reload()
